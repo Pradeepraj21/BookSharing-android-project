6ca731df7b9e678880712a67997bcf976df3db8f
pradeepraj <nadarpradeepraj21@gmail.com>
2023-01-17 00:59:30 +0530

    updated v0.5

---
+++ app/src/main/java/com/siestech/booksharing/Model/User.java
@@ -0,0 +1,67 @@
+package com.siestech.booksharing.Model;
+
+public class User {
+    private String name, profession, email, password;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    private String image;
+
+    private String userId;
+
+    public User(String name, String profession, String email, String password) {
+        this.name = name;
+        this.profession = profession;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
